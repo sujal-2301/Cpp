6c81579831333b2b55dcd5188b7036ad62cdf232
Sujal Kumar Sinha <sujalksinha23@gmail.com>
2023-03-26 13:05:05 +0530

    added comments

--- test.cpp
+++ test.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
-using namespace std;
-int main()
+#include <iostream>  //for using cout and cin functions from std library
+using namespace std; // dont have to write std::cout instead can write cout directly
+
+int main() // main function of the program
+
 {
-    cout << "Hello world!!";
+    cout << "Hello world!!"; // << is used for output and we can write many lines one after another
     cout << "Output test"
-         << "\n"
+         << "\n" // new line character for newline
          << "Hey";
+
     int x;
     int y;
     x = 29;
-    cin >> y;
+
+    cin >> y; // cin uses >> unlike cout , keep in mind
     cout << "value of x is " << x << " and value of y is" << y;
 
-    return 0;
+    return 0; // returning 0 as our main function is giving out int in definition
 }
