ed553bf422a4f866779735b7d37a5c36b49701bd
Sujal Kumar Sinha <sujalksinha23@gmail.com>
2023-03-26 12:56:10 +0530

    basic I/O in cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello world!!";
+    cout << "Output test"
+         << "\n"
+         << "Hey";
+    int x;
+    int y;
+    x = 29;
+    cin >> y;
+    cout << "value of x is " << x << " and value of y is" << y;
+
+    return 0;
+}
